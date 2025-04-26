f34fb71853f422b298aa26d9b56f048705ab8609
yazan6546 <1210145@student.birzeit.edu>
2025-04-26 19:37:22 +0300

    Update main function to load configuration and print settings

--- src/main.c
+++ src/main.c
@@ -1,6 +1,17 @@
 #include "config.h"
 
-int main() {
+int main(int argc, char *argv[]) {
 
-      printf("Hello, World!\n");
+    Config config;
+
+    if (load_config("../config.txt", &config) == -1) {
+        fprintf(stderr, "Failed to load configuration.\n");
+        return 1;
+        printf("Hello, World!\n");
+    }
+
+    print_config(&config);
+
+
+
 }
