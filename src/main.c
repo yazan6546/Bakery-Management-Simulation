106a877ffa44b7a77d97ed834e3ad1d3d8600ba6
yazan6546 <1210145@student.birzeit.edu>
2025-04-26 20:15:53 +0300

    Add new executables for chefs, bakers, sellers, and customers; update main process management

--- src/main.c
+++ src/main.c
@@ -98,67 +98,21 @@ int main(int argc, char *argv[]) {
 
     // Initialize game state
     init_game(shared_game);
-    //
+
     pid_t pid_graphics = start_process("./graphics", &config);
     pid_t pid_chefs = start_process("./chefs", &config);
     pid_t pid_bakers = start_process("./bakers", &config);
     pid_t pid_sellers = start_process("./sellers", &config);
     pid_t pid_supply_chain = start_process("./supply_chain", &config);
+    pid_t pid_customer = start_process("./customer", &config);
 
-    char gui_pid_str[16];
-    sprintf(gui_pid_str, "%d", pid_graphics);
 
     // Setup signal handler for time management
     signal(SIGALRM, handle_alarm);
     signal(SIGINT, handle_sigint); // Renamed to match actual signal
     alarm(1);  // Start the timer
 
-    int status_referee, status_graphics, status_chefs, status_bakers, status_sellers;
-    int status_supply_chain;
-    waitpid(pid_graphics, &status_referee, 0);
-    waitpid(pid_chefs, &status_graphics, 0);
-    waitpid(pid_bakers, &status_chefs, 0);
-    waitpid(pid_sellers, &status_bakers, 0);
-    waitpid(pid_supply_chain, &status_sellers, 0);
-    waitpid(pid_supply_chain, &status_supply_chain, 0);
-
-    if (WIFEXITED(status_referee)) {
-        printf("Referee Child process exited with code: %d\n", WEXITSTATUS(status_referee));
-    } else if (WIFSIGNALED(status_referee)) {
-        printf("Referee Child process terminated by signal: %d\n", WTERMSIG(status_referee));
-    }
-
-    if (WIFEXITED(status_graphics)) {
-        printf("Graphics Child process exited with code: %d\n", WEXITSTATUS(status_graphics));
-    } else if (WIFSIGNALED(status_graphics)) {
-        printf("Graphics Child process terminated by signal: %d\n", WTERMSIG(status_graphics));
-
-        return 0; // cleanup_resources will be called automatically via atexit
-    }
-
-    if (WIFEXITED(status_chefs)) {
-        printf("Chefs Child process exited with code: %d\n", WEXITSTATUS(status_chefs));
-    } else if (WIFSIGNALED(status_chefs)) {
-        printf("Chefs Child process terminated by signal: %d\n", WTERMSIG(status_chefs));
-    }
-
-    if (WIFEXITED(status_bakers)) {
-        printf("Bakers Child process exited with code: %d\n", WEXITSTATUS(status_bakers));
-    } else if (WIFSIGNALED(status_bakers)) {
-        printf("Bakers Child process terminated by signal: %d\n", WTERMSIG(status_bakers));
-    }
-
-    if (WIFEXITED(status_sellers)) {
-        printf("Sellers Child process exited with code: %d\n", WEXITSTATUS(status_sellers));
-    } else if (WIFSIGNALED(status_sellers)) {
-        printf("Sellers Child process terminated by signal: %d\n", WTERMSIG(status_sellers));
-    }
-
-    if (WIFEXITED(status_supply_chain)) {
-        printf("Supply Chain Child process exited with code: %d\n", WEXITSTATUS(status_supply_chain));
-    } else if (WIFSIGNALED(status_supply_chain)) {
-        printf("Supply Chain Child process terminated by signal: %d\n", WTERMSIG(status_supply_chain));
-    }
+    wait(&pid_graphics);
 
 }
 
@@ -174,7 +128,7 @@ pid_t start_process(const char *binary, Config *config) {
         // Now pass two arguments: shared memory fd and GUI pid.
         char buffer[50];
         serialize_config(config, buffer);
-        if (execl(binary, "./referee", buffer, NULL)) {
+        if (execl(binary, binary, buffer, NULL)) {
             perror("execl referee");
             exit(EXIT_FAILURE);
         }
