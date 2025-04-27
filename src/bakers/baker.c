768f4c58a5ae8884a95b2b9770cc5b8b1d604d17
yazan Abualoun <1210145@student.birzeit.edu>
2025-04-27 15:27:54 +0300

    Refactor bakery simulation to use shared memory and update game initialization

--- src/bakers/baker.c
+++ src/bakers/baker.c
@@ -3,18 +3,28 @@
 #include <time.h>
 #include <unistd.h>
 
-#include "config.h"
+#include "queue.h"
 #include "oven.h"
 #include "BakerTeam.h"
 #include "BakeryQueue.h"
+#include "game.h"
+#include <sys/mman.h>
 
-int main() {
-    Config config;
+Game *game;
 
-    if (load_config("config.txt", &config) != 0) {
-        printf("Failed to load config file.\n");
+int main(int argc, char *argv[]) {
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <fd>\n", argv[0]);
         return 1;
     }
+
+    int fd = atoi(argv[1]);  // 4th argument for mmap
+
+    // Open the file descriptor for reading
+    game = mmap(NULL, sizeof(Game), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    fflush(stdout);
+    Config config = game->config;
 
     print_config(&config);
 
@@ -28,13 +38,12 @@ int main() {
 
     // Create baker teams
     BakerTeam teams[3];
+
     init_team(&teams[0], "Bake Bread");
     init_team(&teams[1], "Bake Cakes and Sweets");
     init_team(&teams[2], "Bake Sweet and Savory Patisseries");
 
-    // Create bakery global queue
-    BakeryQueue bakery_queue;
-    init_bakery_queue(&bakery_queue);
+    queue *baker_queue = createQueue(sizeof(BakeryItem));
 
     // Simulate 20 time units
     for (int t = 0; t < 20; t++) {
@@ -45,32 +54,35 @@ int main() {
             if (rand() % 2 == 0) {
                 char item_name[50];
                 sprintf(item_name, "Item-%d", rand() % 100);
-                enqueue_bakery_item(&bakery_queue, item_name, teams[i].team_name);
+                BakeryItem item = {item_name, teams[i].team_name};
+                enqueue(baker_queue, &item);
                 printf("Team %s produced %s\n", teams[i].team_name, item_name);
             }
         }
 
         // Try to put bakery items into ovens
-        BakeryItem *item = dequeue_bakery_item(&bakery_queue);
-        while (item != NULL) {
+        BakeryItem item;
+        queue *temp = front(baker_queue, &item);
+        while (temp != NULL) {
             int placed = 0;
             for (int j = 0; j < config.NUM_OVENS; j++) {
                 if (!ovens[j].is_busy) {
                     int baking_time = config.MIN_OVEN_TIME + rand() % (config.MAX_OVEN_TIME - config.MIN_OVEN_TIME + 1);
-                    put_item_in_oven(&ovens[j], item->name, item->team_name, baking_time);
-                    printf("Placed %s in Oven %d for %d seconds.\n", item->name, ovens[j].id, baking_time);
+                    put_item_in_oven(&ovens[j], item.name, item.team_name, baking_time);
+                    printf("Placed %s in Oven %d for %d seconds.\n", item.name, ovens[j].id, baking_time);
                     placed = 1;
                     break;
                 }
             }
             if (!placed) {
-                printf("All ovens busy, %s is waiting.\n", item->name);
-                return_item_front_bakery(&bakery_queue, item);
+                printf("All ovens busy, %s is waiting.\n", item.name);
                 break; // stop trying more items this second
-            } else {
-                free(item);
             }
-            item = dequeue_bakery_item(&bakery_queue);
+
+            if (dequeue(baker_queue, &item) == NULL) {
+                printf("deque failed");
+                exit(EXIT_FAILURE);
+            }
         }
 
         // Update ovens (tick)
