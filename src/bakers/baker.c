457d09fcdce4fe10ef7596033ff30e7dedb9a7f0
yazan Abualoun <1210145@student.birzeit.edu>
2025-04-27 18:27:36 +0300

    Update simulation time step logic and increase MAX_TIME configuration

--- src/bakers/baker.c
+++ src/bakers/baker.c
@@ -46,7 +46,7 @@ int main(int argc, char *argv[]) {
     queue *baker_queue = createQueue(sizeof(BakeryItem));
 
     // Simulate 20 time units
-    for (int t = 0; t < 20; t++) {
+    for (int t = 0 ;; t++) {
         printf("\n--- Time step %d ---\n", t + 1);
 
         // Teams randomly produce items
@@ -56,7 +56,6 @@ int main(int argc, char *argv[]) {
                 sprintf(item_name, "Item-%d", rand() % 100);
                 BakeryItem item;
                 backery_item_create(&item, item_name, teams[i].team_name);
-                printf("%s from queue %s\n", item.name, item.team_name);
                 enqueue(baker_queue, &item);
                 printf("Team %s produced %s\n", teams[i].team_name, item_name);
                 fflush(stdout);
@@ -70,16 +69,16 @@ int main(int argc, char *argv[]) {
             for (int j = 0; j < config.NUM_OVENS; j++) {
                 if (!ovens[j].is_busy) {
                     int baking_time = config.MIN_OVEN_TIME + rand() % (config.MAX_OVEN_TIME - config.MIN_OVEN_TIME + 1);
+                    dequeue(baker_queue, &item); // Remove it after placing
                     put_item_in_oven(&ovens[j], item.name, item.team_name, baking_time);
                     printf("Placed %s in Oven %d for %d seconds.\n", item.name, ovens[j].id, baking_time);
-                    dequeue(baker_queue, &item); // Remove it after placing
                     placed = 1;
                     break;
                 }
             }
             if (!placed) {
                 printf("All ovens busy, %s is waiting.\n", item.name);
-                break; // stop trying more items if ovens are full
+                break;
             }
         }
 
