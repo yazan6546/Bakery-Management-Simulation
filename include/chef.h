ec07b3e77ce06468205cdfa97a003704adfe989f
ahmadqaimari <ahmadqaimari@gmail.com>
2025-05-03 03:16:35 +0300

    Edited chef.c to include new function declarations

--- include/chef.h
+++ include/chef.h
@@ -7,6 +7,7 @@
 
 #include <semaphore.h>
 #include <inventory.h>
+#include "products.h"
 
 // Message structure for restock requests
 typedef struct {
@@ -37,6 +38,54 @@ typedef struct {
     sem_t* ready_products_sem;
 } ChefState;
 
+
+#define MAX_CHEFS 10
+#define MAX_ITEMS_QUEUE 100
+
+
+typedef enum {
+    TEAM_PASTE,
+    TEAM_BREAD,
+    TEAM_CAKES,
+    TEAM_SANDWICHES,
+    TEAM_SWEETS,
+    TEAM_SWEET_PATISSERIES,
+    TEAM_SAVORY_PATISSERIES,
+    TEAM_COUNT
+} ChefTeam;
+
+typedef struct {
+    OrderItem item;
+    ChefTeam source_team;
+} PreparedItem;
+
+typedef struct {
+    long mtype;
+    PreparedItem prepared_item;
+} ChefMessage;
+
+typedef struct {
+    int id;
+    ChefTeam team;
+    pid_t pid;
+    int is_active;
+    int items_produced;
+    ProductCategory* specialization;
+    sem_t* inventory_sem;
+    sem_t* ready_products_sem;
+} Chef;
+
+typedef struct {
+    Chef chefs[MAX_CHEFS];
+    int chef_count;
+    int msg_queue_chefs;    // Queue for communication with chefs
+    int msg_queue_bakers;   // Queue for communication with baker manager
+    ProductCatalog* product_catalog;
+    sem_t* inventory_sem;
+    sem_t* ready_products_sem;
+} ChefManager;
+
+
 // Constants for inventory management
 #define LOW_INGREDIENT_THRESHOLD 10
 #define RESTOCK_TARGET_QUANTITY 50
@@ -45,5 +94,14 @@ typedef struct {
 void check_and_request_ingredients(ChefState *chef, Inventory *inventory);
 void check_for_confirmations(ChefState *chef);
 void prepare_recipes(ChefState *chef, Inventory *inventory, ReadyProducts *ready_products);
-
+ChefManager* init_chef_manager(ProductCatalog* catalog, sem_t* inv_sem, sem_t* ready_sem);
+void start_chef(Chef* chef, int msg_queue_id);
+void process_chef_messages(ChefManager* manager, int* team_queues);
+ChefTeam get_team_for_product_type(ProductType type);
+ProductType get_product_type_for_team(ChefTeam team);
+void simulate_chef_work(ChefTeam team, int msg_queue_id);
+void calculate_production_ratios(const ReadyProducts *ready_products, float *ratios);
+void reallocate_chefs(ChefManager* manager, int* team_queues, float* ratios);
+void balance_teams(ChefManager *manager);
+void handle_team_change(int signum);
 #endif //CHEF_H
