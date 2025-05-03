bdef745c02b1ddf48dc8c9b9550e35fc6dd5a628
ghazicc <ghazihajqassem@gmail.com>
2025-05-03 22:05:02 +0300

    Add products_utils.c to baker_worker executable and comment out baker path in game_init

--- src/game.c
+++ src/game.c
@@ -30,7 +30,7 @@ int game_init(Game *game, pid_t *processes, int shared_mem_fd) {
     char *binary_paths[] = {
         //"./graphics",
         //"./chefs",
-        "./bakers",
+        // "./bakers",
         //"./sellers",
         "./supply_chain_manager"
         // "./customer_manager"
